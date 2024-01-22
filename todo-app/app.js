9d71cd5f8481dde8f886acb18ae0e036439ea59b
RSHV-CODER <rishav.202204005@tulas.edu.in>
2024-01-23 02:07:45 +0530

    ...

--- todo-app/app.js
+++ todo-app/app.js
@@ -5,40 +5,29 @@ const { Todo } = require("./models");
 const path = require("path");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-
-app.get("/todos", function (request, response) {
-  console.log("Todo List", request.body);
-});
 app.set("view engine", "ejs");
 
-app.get("/", async (request,response) =>{
-  const allTodos = await Todo.getTodos();
-  if(request.accepts("html")){
-    response.render('index',
-    {allTodos});
-    
-  }else{
-    response.json({
-      allTodos
-    })
-  }
-  
-})
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname,'public')));
+app.get("/", async (request, response) => {
+  try {
+    const allTodos = await Todo.getTodos();
+    if (request.accepts("html")) {
+      response.render('index', { allTodos });
+    } else {
+      response.json({ allTodos });
+    }
+  } catch (error) {
+    console.log(error);
+    response.status(422).json(error);
+  }
+});
 
 app.get("/todos", async function (_request, response) {
-  console.log("Processing list of all Todos ...");
-  // FILL IN YOUR CODE HERE
-
-  // First, we have to query our PostgerSQL database using Sequelize to get list of all Todos.
-  // Then, we have to respond with all Todos, like:
-  // response.send(todos)
-  try{
+  try {
     const todo = await Todo.findAll();
     return response.json(todo);
-
-  } catch(error){
+  } catch (error) {
     console.log(error);
     return response.status(422).json(error);
   }
@@ -65,8 +54,8 @@ app.post("/todos", async function (request, response) {
 });
 
 app.put("/todos/:id/markAsCompleted", async function (request, response) {
-  const todo = await Todo.findByPk(request.params.id);
   try {
+    const todo = await Todo.findByPk(request.params.id);
     const updatedTodo = await todo.markAsCompleted();
     return response.json(updatedTodo);
   } catch (error) {
@@ -76,14 +65,13 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 });
 
 app.delete("/todos/:id", async function (request, response) {
-  console.log("We have to delete a Todo with ID: ", request.params.id);
-  // FILL IN YOUR CODE HERE
-
-  // First, we have to query our database to delete a Todo by ID.
-  // Then, we have to respond back with true/false based on whether the Todo was deleted or not.
-  // response.send(true)
-  const deleteFlag = await Todo.destroy({ where: { id: request.params.id}})
-   response.send(deleteFlag?true:false)
+  try {
+    const deleteFlag = await Todo.destroy({ where: { id: request.params.id } });
+    response.send(deleteFlag ? true : false);
+  } catch (error) {
+    console.log(error);
+    response.status(422).json(error);
+  }
 });
 
 module.exports = app;
--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,28 +1,25 @@
 "use strict";
 const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
 
     static addTodo({ title, dueDate }) {
-      return this.create({ title: title, dueDate: dueDate, completed: false });
+      return this.create({ title, dueDate, completed: false });
     }
 
     static getTodos() {
       return this.findAll();
     }
 
-    markAsCompleted() {
-      return this.update({ completed: true });
+    static markAsCompleted(id) {
+      return this.update({ completed: true }, { where: { id } });
     }
   }
+
   Todo.init(
     {
       title: DataTypes.STRING,
@@ -34,5 +31,6 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "Todo",
     }
   );
+
   return Todo;
 };
